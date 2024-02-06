c47c10f3dbdd37a203c4bac2c3186500ee8020d9
abhayantino <abhay.s@antino.com>
2024-02-06 09:53:52 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,43 @@ import squidGameLogo from "./images/logo.png"
 import gunman from "./images/man.png"
 import imdb from "./images/imdb.png"
 import { IoMdTrendingUp } from "react-icons/io";
+import background from "./images/Background.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Header/>
-        <div className=' flex flex-row gap-28 px-20'>
-        <div className='flex flex-col'>
-        <img 
-        className='object-scale-down'
-        src={squidGameLogo} alt="" />
-        <div className=''>
-        Life is like a game, there are many players. 
-        If you don’t play with them, they’ll play with you.
-        </div>
-        <div className='flex flex-row gap-1'>
-        <IoMdTrendingUp/>
-          Trending #1
-        </div>
-        <div className='text-center my-12'>
-          <button className=" bg-red-600 rounded-xl">
-            continue watching 
-          </button>
-          <div className='pt-9'>
-          <div className='flex flex-row' >
-            S1 E9 2021<div> <img src={imdb} alt="" /> </div>
-            <div>8.2</div>
+      <div className=' bg-gradient-to-r from-pink-500 to-pink-800 h-'>
+        <img className='w-full h-100% absolute '
+        src={background} alt="" />
+        <Header />
+        <div className=' flex flex-row gap-10 px-20'>
+          <div className='flex flex-col '>
+            <img
+              className='object-scale-down '
+              src={squidGameLogo} alt="" />
+            <div className='px-24 text-white'>
+              Life is like a game, there are many players.
+              If you don’t play with them, they’ll play with you.
+            </div>
+            <div className='flex flex-row gap-1 px-24 py-2' >
+              <div className='py-1'><IoMdTrendingUp /></div>
+              Trending #1
+            </div>
+            <div className='text-center my-12'>
+              <button className=" bg-gradient-to-r from-red-500 to-red-700 rounded-xl px-3 py-1">
+                continue watching
+              </button>
+              <div className='pt-9 '>
+                <div className='flex flex-row center justify-center gap-5' >
+                  S1 E9 2021<div className='py-1'> <img src={imdb} alt="" /> </div>
+                  <div>8.2</div>
+                </div>
+
+              </div>
+            </div>
           </div>
-          
-          </div>
-        </div>
-          </div>
-        <img src={gunman} alt="" />
+          <img src={gunman} alt="" />
         </div>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import { IoArrowBackOutline } from "react-icons/io5";
 function Header() {
     return (
       
-        <div className='flex flex-row px-40 py-6 '>
+        <div className='flex flex-row px-40 py-6 bg-gradient-to-r from-red-700 to-red-300'>
             <div className='flex items-center '><IoArrowBackOutline  /></div>
-            <ul className=' flex flex-row items-center gap-10 px-56 pl'>
-                <li className=''>Home</li>
-                <li className='text-red-600'>about</li>
-                <li>Cast</li>
-                <li>Trailer</li>
+            <ul className=' flex flex-row items-center gap-10 px-56'>
+                <li className=' hover:bg-red-500 p-2 rounded-md'>Home</li>
+                <li className='text-red-800 hover:bg-white p-2 rounded-md'>about</li>
+                <li className=' hover:bg-red-500 p-2 rounded-md'>Cast</li>
+                <li className=' hover:bg-red-500 p-2 rounded-md'>Trailer</li>
             </ul>
             <div> <img className="object-scale-down "
             src=" 
